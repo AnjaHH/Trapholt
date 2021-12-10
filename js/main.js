09d1b8e3be7ef4757074b6fc45f5fb3a697ec594
AnjaHein <94353080+AnjaHH@users.noreply.github.com>
2021-12-10 09:36:56 +0100

    9/12

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(function(){
+    $("#onetest").hover(function(){
+        $("#main").toggleClass("greenBackground");
+    });
+});
+
+$(function(){
+    $("#twotest").hover(function(){
+        $("#main").toggleClass("yellowBackground");
+    });
+});
+
+$(function(){
+    $("#threetest").hover(function(){
+        $("#main").toggleClass("pinkBackground");
+    });
+});
+
+$(function(){
+    $("#fourtest").hover(function(){
+        $("#main").toggleClass("blueBackground");
+    });
+});
+$(function(){
+    $("#fivetest").hover(function(){
+        $("#main").toggleClass("greenBackground");
+    });
+});
+
+$(function(){
+    $("#sixtest").hover(function(){
+        $("#main").toggleClass("yellowBackground");
+    });
+});
+$(function(){
+    $("#seventest").hover(function(){
+        $("#main").toggleClass("pinkBackground");
+    });
+});
+
+$(function(){
+    $("#eighttest").hover(function(){
+        $("#main").toggleClass("blueBackground");
+    });
+});
+$(function(){
+    $("#ninetest").hover(function(){
+        $("#main").toggleClass("greenBackground");
+    });
+});
+
+$(function(){
+    $("#tentest").hover(function(){
+        $("#main").toggleClass("yellowBackground");
+    });
+});
